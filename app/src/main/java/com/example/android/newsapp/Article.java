315882d087f052f6ca7a4c9a9bcd872ae05848aa
Tan Nguyen <ban5hien@yahoo.com>
2018-06-05 17:56:56 -0700

    add an Article class

---
+++ app/src/main/java/com/example/android/newsapp/Article.java
@@ -0,0 +1,37 @@
+package com.example.android.newsapp;
+
+public class Article {
+    private String mSection;
+    private String mTitle;
+    private String mAuthor;
+    private String mDate;
+    private String mUrl;
+
+    public Article(String section, String title, String author, String date, String url){
+        mSection = section;
+        mTitle = title;
+        mAuthor= author;
+        mDate = date;
+        mUrl = url;
+    }
+
+    public String getmSection() {
+        return mSection;
+    }
+
+    public String getmAuthor() {
+        return mAuthor;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+}
