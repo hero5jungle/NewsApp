8cba2246497d4303121792030c910ecde5f758c2
Tan Nguyen <ban5hien@yahoo.com>
2018-06-06 14:58:23 -0700

    fill out the article activity

--- app/src/main/java/com/example/android/newsapp/ArticleActivity.java
+++ app/src/main/java/com/example/android/newsapp/ArticleActivity.java
@@ -1,13 +1,124 @@
 package com.example.android.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class ArticleActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<Article>> {
+
+    private static final String LOG_TAG = ArticleActivity.class.getName();
+
+    private static final String GUARDIAN_REQUEST_URL =
+            "https://content.guardianapis.com/search?api-key=test";
+
+    private static final int ARTICLE_LOADER_ID = 1;
+    private ArticleAdapter mAdapter;
+    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.article_list_item);
+        setContentView(R.layout.article_activity);
+
+        ListView articleListView = (ListView) findViewById(R.id.list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        articleListView.setEmptyView(mEmptyStateTextView);
+
+        mAdapter = new ArticleAdapter(this, new ArrayList<Article>());
+
+        articleListView.setAdapter(mAdapter);
+
+        articleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current article that was clicked on
+                Article currentArticle = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri articleUri = Uri.parse(currentArticle.getmUrl());
+
+                // Create a new intent to view the article URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, articleUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+
+    @Override
+    public Loader<List<Article>> onCreateLoader(int i, Bundle bundle) {
+
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "json");
+        uriBuilder.appendQueryParameter("order-by", "newest");
+
+        return new ArticleLoader(this, uriBuilder.toString());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Article>> loader, List<Article> articles) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No articles found."
+        mEmptyStateTextView.setText(R.string.no_article);
+
+        if (articles != null && !articles.isEmpty()) {
+            mAdapter.addAll(articles);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Article>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
     }
 }
