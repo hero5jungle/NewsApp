06f9108b8a76448851f63c86871f8b8d7c9e2b20
Tan Nguyen <ban5hien@yahoo.com>
2018-06-05 18:05:23 -0700

    refactor main activity name

--- app/src/main/java/com/example/android/newsapp/ArticleActivity.java
+++ app/src/main/java/com/example/android/newsapp/ArticleActivity.java
@@ -3,7 +3,7 @@ package com.example.android.newsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ArticleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
